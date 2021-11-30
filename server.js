d20672baba85ccb4c5ca9cd18539ac801d59fec8
amsriniva2001 <59710877+amsriniva2001@users.noreply.github.com>
2021-11-30 01:52:56 -0500

    push

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get("/app/users", (req, res) => {
 	//console.log(info.changes); 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
-	db.prepare('DELETE FROM userinfo WHERE id = ?').run();
+	res.prepare('DELETE FROM userinfo WHERE id = ?').run();
 	res.status(200).json({"message":"1 record deleted: (200)"});
 });
 // Default response for any other request
