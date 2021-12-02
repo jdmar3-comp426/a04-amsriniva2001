8971fa08486535f4736b11b1827129a19d51d7fb
amsriniva2001 <59710877+amsriniva2001@users.noreply.github.com>
2021-12-02 15:38:07 -0500

    push

--- server.js
+++ server.js
@@ -37,14 +37,16 @@ app.get("/app/users/", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-app.get("/app/users/:req.params.id", (req, res) => {
+app.get("/app/users/:id", (req, res) => {
 	 const stmt = db.prepare("SELECT * FROM userinfo WHERE id = " + req.params.id).all();
 	 res.status(200).json(stmt[0]);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
+app.patch("/app/update/user/:id", (req,res) => {
 	const stmt = db.prepare('UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?');
 	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
 	res.status(200).json({"message":"1 record updated: ID" + req.params.id + " (200)"}); 
+});
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare('DELETE FROM userinfo WHERE id = ?');
